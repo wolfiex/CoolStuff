176518610b6ef92029dc1988f7ffa301951f5639
Dan Ellis <daniel.ellis.research@gmail.com>
2016-10-07 15:57:29 +0100

    mov2gif

---
+++ Vis/mov2gif.sh
@@ -0,0 +1,6 @@
+ffmpeg -y -ss 30 -t 3 -i $@ \
+-vf fps=10,scale=320:-1:flags=lanczos,palettegen palette.png\
+ &&  \
+ffmpeg -ss 30 -t 3 -i $@ -i palette.png -filter_complex \
+ "fps=10,scale=320:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif
+
