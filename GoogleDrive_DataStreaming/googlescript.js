89055c9b8bbbe9f43ea98c6866193e8c0e302db5
wolfiex <daniel.ellis.research@gmail.com>
2017-02-08 12:32:24 +0000

    Update googlescript.js
    lets not rewrite everything...

--- GoogleDrive_DataStreaming/googlescript.js
+++ GoogleDrive_DataStreaming/googlescript.js
@@ -24,7 +24,7 @@ function importData() {
     //var newsheet = ss.insertSheet('NEWDATA'); // create a 'NEWDATA' sheet to store imported data
     var newsheet = ss.getSheetByName('NEWDATA');
     // loop through csv data array and insert (append) as rows into 'NEWDATA' sheet
-    for ( var i=0, lenCsv=csvData.length; i<lenCsv; i++ ) {
+    for ( var i=newsheet.getLastRow()+1, lenCsv=csvData.length; i<lenCsv; i++ ) {
       newsheet.getRange(i+1, 1, 1, csvData[i].length).setValues(new Array(csvData[i]));
     }
       }
