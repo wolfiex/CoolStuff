2e25b7d6e418c6b067d532fbe0b4c1dc2c6d9b2b
wolfiex <daniel.ellis.research@gmail.com>
2017-03-16 16:40:04 +0000

    Update JuliaInstalls.jl

--- JuliaInstalls.jl
+++ JuliaInstalls.jl
@@ -11,5 +11,24 @@ Pkg.add("NetCDF")
 
 
 ## node interface
-npm install node-gyp
+npm install -g node-gyp
 npm install node-julia
+
+
+#last stable node 5.2 for julia linking 
+One way is to use NVM, the Node Version Manager. You can find it at https://github.com/creationix/nvm
+
+It allows you to easily install and manage multiple versions of node. Here's a snippet from the help:
+
+Usage:
+nvm install <version>       Download and install a <version>
+nvm use <version>           Modify PATH to use <version>
+nvm ls                      List versions (installed versions are blue)
+
+install: 
+curl https://raw.githubusercontent.com/creationix/nvm/v0.19.0/install.sh | bash
+add . ~/.nvm/nvm.sh to bash profile 
+http://flnkr.com/2014/11/install-nvm-and-node-js-on-os-x/
+
+
+
