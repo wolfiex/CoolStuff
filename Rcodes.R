4e42170f38f6cff3bf3489e6d7b84062b96fb934
wolfiex <daniel.ellis.research@gmail.com>
2017-03-31 00:55:52 +0100

    Create Rcodes.R

---
+++ Rcodes.R
@@ -0,0 +1,13 @@
+
+packages <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg))
+        install.packages(new.pkg, dependencies = TRUE, repos='https://cran.rstudio.com/')
+    sapply(pkg, require, character.only = TRUE)
+}
+
+packages(c("foo", "bar", "baz"))
+}
+
+source('myfile.R')
+
